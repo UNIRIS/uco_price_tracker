fac2fcb190705903b009a8e1e2112332831fca10
Samuel Manzanera <samuel@uniris.io>
2020-08-07 14:08:45 +0200

    add websockets

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const { getUCOPrice } = require('./blockchainio_api')
 const { getBTCPrice } = require('./btc_api')
 
-const express = require('express')
-const app = express()
+const app = require('express')()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get("/uco_price/:currency", async (req, res) => {
-    try {
-        const uco_price = await getUCOPrice()
-        const btc_price = await getBTCPrice(req.params.currency)
-    
-        res.json({ price: btc_price * uco_price, currency: req.params.currency })
-    }
-    catch (e) {
-        res.json({ error: e.message })
-    }
+const port = process.env["UCO_TRACKER_PORT"] || 3000
+
+const getPrice = function (currency) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const uco_price = await getUCOPrice()
+            const btc_price = await getBTCPrice(currency)
+            return resolve({ price: btc_price * uco_price, currency: currency })
+        }
+        catch (e) {
+            console.log(e)
+            return reject(e.message)
+        }
+    })
+}
+
+io.on('connection', (socket) => {
+    socket.on("uco_price", (currency) => {
+        setInterval(() => {
+            getPrice(currency)
+            .then(price => {
+                socket.emit("uco_price", price)
+            })
+        }, 3000)
+    })
 })
 
-app.listen(3000)
+app.get("/uco_price/:currency", (req, res) => {
+    getPrice(req.params.currency)
+        .then(price => {
+            res.json(price)
+        })
+        .catch((e) => {
+            res.json({ error: e})
+        })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(port, () => {
+    console.log(`Application listening on ${port}`)
+})
