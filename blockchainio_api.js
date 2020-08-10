b0207638198e27d66c2900633e764c8ff159db73
Samuel Manzanera <samuel@uniris.io>
2020-08-10 13:01:35 +0200

    add some error handling

--- blockchainio_api.js
+++ blockchainio_api.js
@@ -3,9 +3,13 @@ const API = "https://api.blockchain.io/v1/ticker/price?symbol=ucobtc"
 
 module.exports.getUCOPrice = function() {
     return new Promise((resolve, reject) => {
-        request.get(API, { json: true}, (err, res, body) => {
+        request.get(API, { json: true,      strictSSL : false        }, (err, res, body) => {
             if (err) { 
                 return reject(err)
+            }
+            if (isNaN(parseFloat(body.price))) {
+                setTimeout(function(){return true;},1000);
+                return getUCOPrice()
             }
             resolve(parseFloat(body.price))
         })
