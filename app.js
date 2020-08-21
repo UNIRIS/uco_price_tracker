b1e0299a74d34885922c91bc33a5109b6dc06107
Samuel Manzanera <samuel@uniris.io>
2020-08-21 11:38:48 +0200

    add logging capability of the price

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const cors = require('cors')
 
 app.use(cors())
 
+const path = require('path')
 const http = require('http')
 const https = require('https')
 const fs = require('fs')
+const CronJob = require('cron').CronJob;
+const readLastLines = require('read-last-lines');
 
-http.createServer(app).listen(5000)
+const server = http.createServer(app).listen(5000)
 
 const server = https.createServer({
     key: fs.readFileSync(process.env["SSL_KEY"]),
@@ -21,7 +24,7 @@ const io = require('socket.io')(server);
 
 const port = process.env["UCO_TRACKER_PORT"] || 3000
 
-const getPrice = function (currency) {
+const searchPrice = function (currency) {
     return new Promise(async (resolve, reject) => {
         try {
             const uco_price = await getUCOPrice()
@@ -29,7 +32,7 @@ const getPrice = function (currency) {
             const btc_price = await getBTCPrice(currency)
             console.log(`BTC Price in ${currency}: ${btc_price}`)
             console.log(`UCO Price in ${currency}: ${btc_price * uco_price}`)
-            return resolve({ price: btc_price * uco_price, currency: currency })
+            return resolve(btc_price * uco_price)
         }
         catch (e) {
             console.log(e)
@@ -38,25 +41,52 @@ const getPrice = function (currency) {
     })
 }
 
+const log_file = path.join(__dirname, "uco_history_eur")
+
+const putPrice = function(price) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(log_file, `${new Date().toLocaleString()} - ${price}\n`, { flag: 'a+'}, err => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
+
+const getPrice = function() {
+    return new Promise((resolve, reject) => {
+        return readLastLines.read(log_file, 1).then(line => {
+            splitted_entry = line.split(' - ')
+            resolve(parseFloat(splitted_entry[1].replace("\n", "")))
+        })
+    })
+}
+
+const job = new CronJob('10 * * * * *', async () => {
+    price = await searchPrice("EUR")
+    await putPrice(price)
+}, null, true, 'Etc/UTC');
+
+job.start();
+
 io.on('connection', (socket) => {
-    socket.on("uco_price", (currency) => {
+    socket.on("uco_price", () => {
         setInterval(() => {
-            getPrice(currency)
-            .then(price => {
+            getPrice().then(price => {
                 socket.emit("uco_price", price)
             })
-        }, 3000)
+        }, 10000)
     })
 })
 
-app.get("/uco_price/:currency", (req, res) => {
-    getPrice(req.params.currency)
-        .then(price => {
-            res.json(price)
-        })
-        .catch((e) => {
-            res.json({ error: e})
-        })
+app.get("/uco_price", (req, res) => {
+    getPrice().then(price => {
+        res.json(price)
+    })
+    .catch((e) => {
+        res.json({ error: e})
+    })
 })
 
 app.get('/', (req, res) => {
